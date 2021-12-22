da42eee3de7a08c76f62b96c3947776a3fba08c6
Gonzalo Romero <gondragon13@gmail.com>
2021-12-22 19:44:08 -0300

    Reeplace Transpiler Patch with Inheritance and XML patch

---
+++ JobDriver_FillTerrainWithResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using VFEMedieval;
+
+namespace VFEM_MoatPatch
+{
+    class JobDriver_FillTerrainWithResource : JobDriver_FillTerrain
+    {
+        protected override void DoEffect(IntVec3 c)
+        {
+            Mod.Debug("JobDriver_FillTerrainWithResource.DoEffect");
+            TerrainDef terrain = this.TargetLocA.GetTerrain(this.Map);
+            TerrainDef newTerr = TerrainHandler.HandleFill(terrain);
+            this.Map.terrainGrid.SetTerrain(this.TargetLocA, newTerr);
+            FilthMaker.RemoveAllFilth(this.TargetLocA, this.Map);
+        }
+    }
+}
--- VFEM_Patches.cs
+++ VFEM_Patches.cs
@@ -57,47 +57,6 @@ namespace VFEM_MoatPatch
         }
     }
 
-    [HarmonyPatch(typeof(JobDriver_FillTerrain), "DoEffect")]
-    internal class VFEM_JobDriver_FillTerrain_Patch
-    {
-        internal static MethodInfo HandleFill = AccessTools.Method(typeof(TerrainHandler), nameof(TerrainHandler.HandleFill));
-        internal static IEnumerable<CodeInstruction> Transpiler(ILGenerator gen, IEnumerable<CodeInstruction> instructions)
-        {
-            bool found = false;
-            bool patched = false;
-            foreach (CodeInstruction code in instructions)
-            {
-                if (!found)
-                {
-                    if (code.opcode == OpCodes.Callvirt) // Here is where the Switch Case starts
-                    {
-                        Mod.Debug("Callvirt found on Fill");
-                        found = true;
-                    }
-                    else
-                    {
-                        yield return code;
-                    }
-                }
-                else if (!patched)
-                {
-                    if (code.opcode == OpCodes.Ldarg_0) //Insert a call to custom handler after the switch code. TerrainDef is already on the stack.
-                    {
-                        Mod.Debug("Patching on Ldarg_0 on Fill");
-                        yield return new CodeInstruction(OpCodes.Call, HandleFill);
-                        yield return new CodeInstruction(OpCodes.Stloc_1);
-                        yield return code;
-                        patched = true;
-                    }
-                }
-                else
-                {
-                    yield return code;
-                }
-            }
-        }
-    }
-
     [HarmonyPatch]
     internal class VFEM_Designator_FillTerrain_Patch
     {
